8bbcb1fcfaf2f24ccbf86ae7223bf7dad75475c2
Matheus10s10 <72151068+Matheus10s10@users.noreply.github.com>
2021-03-12 12:21:34 -0300

    Add files via upload

---
+++ alunos.sql
@@ -0,0 +1,12 @@
+use mysql;
+
+create table alunos(
+codigo varchar(4),
+nome varchar(30),
+idade varchar(3),
+cpf varchar(11),
+telefone varchar (11),
+endereco varchar(50)
+);
+select * from alunos;
+
